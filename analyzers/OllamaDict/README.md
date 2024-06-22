dfecfc4b313513e083be4f4983a09b7172bf6ec1
David de Hilster <david.dehilster@lexisnexisrisk.com>
2024-06-22 16:03:00 -0400

    LLM-008 Added analyzer for individual word files
    Signed-off-by: David de Hilster <david.dehilster@lexisnexisrisk.com>

---
+++ analyzers/OllamaDict/README.md
@@ -0,0 +1,53 @@
+# Using Local Ollama to Build Dictionary
+
+The idea behind this analyzer is to use a local LLM to generate responses to prompts asking about linguistic information about words in English.
+
+The dictionary being used is from the current full english dictionary in the NLP++ library (en-full.dict) and the English words found in this repository: https://github.com/dwyl/english-words/blob/master/words.txt. The two have been merged into one large file and then split into separate letter files using the python script divideWords.py.
+
+## Current Progress
+
+Currently, David de Hilster is running the python script on his computer and generating the definition files for each letter. This is the output from running the `fileLines.py` python script.
+
+- [ ] Total words: 492935
+- [x] aaa.txt, word count: 8
+- [x] x.txt, word count: 456
+- [x] z.txt, word count: 1240
+- [x] y.txt, word count: 1291
+- [x] j.txt, word count: 2741
+- [x] q.txt, word count: 3013
+- [x] k.txt, word count: 3818
+- [x] v.txt, word count: 5800
+- [x] w.txt, word count: 10223
+- [x] l.txt, word count: 11185
+- [ ] g.txt, word count: 11850
+- [ ] o.txt, word count: 14189
+- [ ] f.txt, word count: 14515
+- [ ] i.txt, word count: 14680
+- [ ] n.txt, word count: 14797
+- [ ] e.txt, word count: 14955
+- [ ] h.txt, word count: 15999
+- [ ] r.txt, word count: 19730
+- [ ] b.txt, word count: 20325
+- [ ] d.txt, word count: 20832
+- [ ] m.txt, word count: 21002
+- [ ] t.txt, word count: 22465
+- [ ] u.txt, word count: 23490
+- [ ] a.txt, word count: 25902
+- [ ] c.txt, word count: 34725
+- [ ] p.txt, word count: 37960
+- [ ] s.txt, word count: 46893
+- [ ] caps.txt, word count: 78851
+
+## Installing Ollama
+
+The current python scripts were developed in a Windows WSL Linux system.
+
+Install the python package using pip:
+
+```
+pip install ollama
+```
+
+## Useful Links
+
+https://developers-blog.org/ollama-python-library-tutorial-with-examples/
---
+++ analyzers/OllamaDict/input/separateWords.py
@@ -0,0 +1,59 @@
+import re
+import os
+
+def write_word_file(input_file, word, def_lines):
+    letter = get_file_name_without_extension(input_file).replace("-def", "")
+    print(f"Writing {word} to {letter} directory")
+    write_letter_word_file(letter, word, def_lines)
+
+def create_directories_if_not_exist(file_path):
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def get_file_name_without_extension(file_path):
+    base_name = os.path.basename(file_path)
+    file_name_without_extension = os.path.splitext(base_name)[0]
+    
+    return file_name_without_extension
+
+def write_letter_word_file(letter, word, lines):
+    filename = f"{word}.txt"
+    file_path = os.path.join(os.getcwd(), "OllamaPrompt", "input", "wordfiles", letter, filename);
+    create_directories_if_not_exist(file_path)
+    with open(file_path, 'w', encoding="utf-8") as file:
+        for line in lines:
+            file.write(line)
+
+def make_word_files(input_file):
+    # Read the input file line by line
+    with open(input_file, 'r', encoding="utf-8") as f:
+        lines = f.readlines()
+
+    # Define the pattern: single quote, text, single quote, at least five "="
+    pattern = re.compile(r"\"([^']+)\"\s*={5,}")
+
+    def_lines = []
+    word = ''
+    foundHeader = False
+    # Iterate through the lines
+    for line in lines:
+        match = pattern.match(line)
+        if match:
+            if len(def_lines) > 0:
+                write_word_file(input_file, word, def_lines)
+                def_lines = []
+
+            word = match.group(1)
+            foundHeader = True
+        elif foundHeader:
+            def_lines.append(line)
+
+    if len(def_lines) > 0:
+        write_word_file(input_file, word, def_lines)
+
+directory = os.path.join(os.getcwd(), "OllamaPrompt", "input", "wordfiles")
+for filename in os.listdir(directory):
+    if filename == "l-def.txt":
+        file_path = os.path.join(directory, filename)
+        make_word_files(file_path)
